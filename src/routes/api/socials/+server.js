d2afa1fc471878c93fd206aa1652663da8e89075
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-05-08 18:54:56 +0530

    simplified socials server

--- src/routes/api/socials/+server.js
+++ src/routes/api/socials/+server.js
@@ -1,9 +1,6 @@
-import Bun from 'bun';
+import data from '../../../../static/data/socials.json';
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
-	const filePath = 'static/data/socials.json';
-	const file = Bun.file(filePath);
-
-	return json(await file.json());
+	return json(data);
 }
