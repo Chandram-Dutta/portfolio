e73e27b8a6561841793ccdbc25e05e3d54f5472e
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-06-06 16:33:16 +0530

    feat: added fib page

---
+++ src/routes/fluttericonbrowser/+page.svelte
@@ -0,0 +1,24 @@
+<div class="flex lg:flex-row flex-col">
+	<img class="h-96 object-contain" src="images/fib_ss.png" alt="Flutter Icon Browser Screenshot" />
+	<div class="flex flex-col items-center justify-center p-8">
+		<div class="flex flex-row items-center p-8">
+			<img class="h-8" src="icons/fib.svg" alt="Flutter Icon Browser Logo" />
+			<div class="pl-4 text-xl lg:text-4xl text-center">Flutter Icon Browser</div>
+		</div>
+		<div class="flex flex-col lg:flex-row justify-center items-center">
+			<a href="https://links.onlychan.xyz/fib-playstore">
+				<button class="btn m-2">Play Store</button>
+			</a>
+			<!-- <button class="btn m-2">App Store</button> -->
+			<a href="https://links.onlychan.xyz/fib">
+				<button class="btn m-2">Web</button>
+			</a>
+			<a href="fluttericonbrowser/privacy-policy">
+				<button class="btn m-2">Privacy Policy</button>
+			</a>
+			<a href="fluttericonbrowser/terms-and-conditions">
+				<button class="btn m-2">Terms & Conditions</button>
+			</a>
+		</div>
+	</div>
+</div>
