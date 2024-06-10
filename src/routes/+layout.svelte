76216d25d14e141df51ddf4f06bde6663cf108b2
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-06-10 19:43:03 +0530

    removed bottom bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,9 +11,9 @@
 <div class="min-h-screen flex justify-center items-center">
 	<div class="px-7 py-24"><slot /></div>
 </div>
-<div
+<!-- <div
 	class="fixed bottom-0 left-0 w-full z-10 p-2 text-sm text-secondary-content flex flex-col md:flex-row justify-between bg-secondary items-center"
 >
 	<div class="text-center">Built using SvelteKit, Tailwind & DaisyUI. Deployed on Vercel.</div>
 	<div>© Chandram Dutta 2024</div>
-</div>
+</div> -->
